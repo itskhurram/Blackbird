14eb8b1c0fa42cbac05c08969c6924d461ff50eb
khurram shahzad <itskhurrams@outlook.com>
2022-05-24 17:39:07 +0500

    Models Added

---
+++ src/Blackbird.Domain/Models/MemoryCacheKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace Blackbird.Domain.Models {
+    public static class MemoryCacheKeys {
+        public static string Entry => "_Entry";
+        public static string Category => "_Category";
+    }
+}
