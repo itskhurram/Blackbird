7d9bd831a33558ed9879aa3d1077d5a972b297e9
khurram shahzad <itskhurrams@outlook.com>
2022-05-27 16:18:28 +0500

    Services Added

--- src/Blackbird.API/Controllers/AccountTypeController.cs
+++ src/Blackbird.API/Controllers/AccountTypeController.cs
@@ -3,5 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blackbird.API.Controllers {
     public class AccountTypeController : BaseController {
+        //private readonly ILeadService _leadService;
+        //public LeadController(ILeadService leadService) {
+        //    _leadService = leadService;
+        //}
+        //[HttpGet]
+        //public async Task<IEnumerable<Lead>> Get() {
+        //    return await _leadService.GetAllLeads(true);
+        //}
     }
 }
