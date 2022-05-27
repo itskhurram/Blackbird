f8fa649c6b421240fc2f2e7b7463579472e724a6
khurram shahzad <itskhurrams@outlook.com>
2022-05-27 17:38:31 +0500

    Memory Cache Implemented

--- src/Blackbird.Infrastructure.IOC/Container/DependencyContainer.cs
+++ src/Blackbird.Infrastructure.IOC/Container/DependencyContainer.cs
@@ -2,6 +2,7 @@
 using Blackbird.Application.Services;
 using Blackbird.Domain.Interfaces;
 using Blackbird.Domain.Interfaces.Base;
+using Blackbird.Infrastructure.Caching.MemoryCache;
 using Blackbird.Infrastructure.Logging.Logger;
 using Blackbird.Infrastructure.Persistance.Repositories;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,16 +17,16 @@ namespace Blackbird.Infrastructure.IOC.Container {
         private static void ServicesRegistration(IServiceCollection services) {
             services.AddTransient<IAccountTypeService, AccountTypeService>();
             services.AddTransient<ICategoryService, CategoryService>();
-            //services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IUserService, UserService>();
             //services.AddTransient<IJWTRefreshTokenService, JWTRefreshTokenService>();
         }
         private static void RepositoryRegistration(IServiceCollection services) {
             services.AddTransient<IBaseRepository, BaseRepository>();
             services.AddTransient<IAccountTypeRepository, AccountTypeRepository>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
-            //services.AddTransient<ILeadRepository, LeadRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
             //services.AddTransient<IJWTRefreshTokenRepository, JWTRefreshTokenRepository>();
-            //services.AddScoped<IMemoryCacheProvider, MemoryCacheProvider>();
+            services.AddScoped<IMemoryCacheProvider, MemoryCacheProvider>();
         }
 
         private static void ApploggerRegistration(IServiceCollection services) {
---
+++ src/Blackbird.Infrastructure.Persistance/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,22 @@
+﻿using Blackbird.Domain;
+using Blackbird.Domain.Interfaces;
+
+namespace Blackbird.Infrastructure.Persistance.Repositories {
+    public class RefreshTokenRepository : IRefreshTokenRepository {
+        public Task<bool> CheckRefreshTokenIsValid(long userId, string refreshToken) {
+            throw new NotImplementedException();
+        }
+
+        public Task<RefreshToken> GetRefreshTokenByUserId(long userId, string refreshTokenKey) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Insert(RefreshToken jWTRefreshToken) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(RefreshToken jWTRefreshToken) {
+            throw new NotImplementedException();
+        }
+    }
+}
