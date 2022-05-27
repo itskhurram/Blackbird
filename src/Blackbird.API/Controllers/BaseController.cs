64157bd4224903c0b9c4f2627dbfe8f8c9e0c26e
khurram shahzad <itskhurrams@outlook.com>
2022-05-27 13:40:30 +0500

    API setup and cleanup

---
+++ src/Blackbird.API/Controllers/BaseController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blackbird.API.Controllers {
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase {
+    }
+}
--- src/Blackbird.API/Program.cs
+++ src/Blackbird.API/Program.cs
@@ -1,25 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
+using Blackbird.Infrastructure.ExceptionHandling;
+using Blackbird.Infrastructure.IOC.Container;
+using Serilog;
+using Serilog.Events;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+try {
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
 
-var app = builder.Build();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddMemoryCache();
+    builder.Services.AddSingleton(builder.Configuration);
+    DependencyContainer.RegisterServices(builder.Services);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging(configure => {
+        configure.MessageTemplate = "HTTP {RequestMethod} {RequestPath} ({UserId}) responded {StatusCode} in {Elapsed:0.0000}ms";
+    });
+    app.UseMiddleware<GlobalExceptionHandling>();
+    if (app.Environment.IsDevelopment()) {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception) {
+    Log.Fatal(exception, "Host terminated unexpectedly");
+    return 1;
+}
+finally {
+    Log.CloseAndFlush();
+}
+return 0;
