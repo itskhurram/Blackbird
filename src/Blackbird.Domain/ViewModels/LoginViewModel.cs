3980c34c5197598dabf1809ce43294a5897414e7
khurram shahzad <itskhurrams@outlook.com>
2022-06-03 16:50:08 +0500

    Vieew Models added

---
+++ src/Blackbird.Domain/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blackbird.Domain.ViewModels {
+    public class LoginViewModel {
+        [Required]
+        public string LoginName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ src/Blackbird.Domain/ViewModels/ResponseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Blackbird.Domain.ViewModels {
+    public class ResponseViewModel {
+        #region Properties and Data Members
+        public object Data { get; set; }
+        public object AdditionalData { get; set; }
+        public string Message { get; set; }
+        public bool OperationStatus { get; set; }
+        #endregion
+    }
+}
