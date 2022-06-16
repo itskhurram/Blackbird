bafd4a8cd5faba7359795051eb86f4d6838ba938
MianAnwar <mianmuhammad.anwar@systemsltd.com>
2022-06-16 16:24:22 +0500

    LawyerCertification model added

---
+++ src/Blackbird.Domain/Entities/LawyerCertifications.cs
@@ -0,0 +1,53 @@
+﻿using Blackbird.Domain.Base;
+
+namespace Blackbird.Domain.Entities
+{
+    public class LawyerCertifications : EntityBase
+    {
+        #region private declarations
+        private long lawyerCertificationId;
+        private string? certificationTitle;
+        private string? certificationCompletionYear;
+        private string? certificationInstitute;
+        private long userId;
+        #endregion
+
+        public long LawyerCertificationId
+        {
+            get => lawyerCertificationId; set
+            {
+                lawyerCertificationId = value;
+                EntityModified();
+            }
+        }
+        public string CertificationTitle
+        {
+            get => certificationTitle; set
+            {
+                certificationTitle = value; EntityModified();
+            }
+        }
+        public string CertificationCompletionYear
+        {
+            get => certificationCompletionYear; set
+            {
+                certificationCompletionYear = value; EntityModified();
+            }
+        }
+        public string CcertificationInstitute
+        {
+            get => certificationInstitute; set
+            {
+                certificationInstitute = value; EntityModified();
+            }
+        }
+        public long UserId
+        {
+            get => userId; set
+            {
+                userId = value;
+                EntityModified();
+            }
+        }
+    }
+}
