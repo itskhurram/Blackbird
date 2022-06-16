96ed88c89099fbe668fddd93af2c9be65721e1d2
MianAnwar <mianmuhammad.anwar@systemsltd.com>
2022-06-16 16:38:12 +0500

    LawyersTopRecommend M Entity Added

---
+++ src/Blackbird.Domain/Entities/LawyersTopRecommend.cs
@@ -0,0 +1,33 @@
+﻿using Blackbird.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackbird.Domain.Entities
+{
+    public class LawyersTopRecommend : EntityBase
+    {
+        #region private declarations
+        private long lawyersTopRecommendId;
+        private long userId;
+        #endregion
+
+        public long LawyersTopRecommendId
+        {
+            get => lawyersTopRecommendId; set
+            {
+                lawyersTopRecommendId = value;
+                EntityModified();
+            }
+        }
+        public long UserId
+        {
+            get => userId; set
+            {
+                userId = value; EntityModified();
+            }
+        }
+    }
+}
