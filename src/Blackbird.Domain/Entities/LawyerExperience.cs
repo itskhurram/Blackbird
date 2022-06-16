61ebfb5c1d2561bfd44acd016e50ee98578f6446
MianAnwar <mianmuhammad.anwar@systemsltd.com>
2022-06-16 16:18:18 +0500

    MembershipPlan, LawyerExperience Models added

--- src/Blackbird.Domain/Entities/LawyerExperience.cs
+++ src/Blackbird.Domain/Entities/LawyerExperience.cs
@@ -1,9 +1,5 @@
 ﻿using Blackbird.Domain.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Blackbird.Domain.Entities
 {
---
+++ src/Blackbird.Domain/Entities/MembershipPlan.cs
@@ -0,0 +1,61 @@
+﻿using Blackbird.Domain.Base;
+
+
+namespace Blackbird.Domain.Entities
+{
+    public class MembershipPlan : EntityBase
+    {
+        #region private declarations
+        private long membershipPlanId;
+        private string? membershipPlanName;
+        private string? feature1;
+        private string? feature2;
+        private string? feature3;
+        private string? feature4;
+        #endregion
+
+        public long MembershipPlanId
+        {
+            get => membershipPlanId; set
+            {
+                membershipPlanId = value;
+                EntityModified();
+            }
+        }
+        public string MembershipPlanName
+        {
+            get => membershipPlanName; set
+            {
+                membershipPlanName = value; EntityModified();
+            }
+        }
+        public string Feature1
+        {
+            get => feature1; set
+            {
+                feature1 = value; EntityModified();
+            }
+        }
+        public string Feature2
+        {
+            get => feature2; set
+            {
+                feature2 = value; EntityModified();
+            }
+        }
+        public string Feature3
+        {
+            get => feature3; set
+            {
+                feature3 = value; EntityModified();
+            }
+        }
+        public string Feature4
+        {
+            get => feature4; set
+            {
+                feature4 = value; EntityModified();
+            }
+        }
+    }
+}
