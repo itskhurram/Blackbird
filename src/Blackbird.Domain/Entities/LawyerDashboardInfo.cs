9b8183058290958d4f57eb338cde9b7fe8cde5e3
MianAnwar <mianmuhammad.anwar@systemsltd.com>
2022-06-17 19:04:31 +0500

    LawyerDashboardInfo Entity added

--- src/Blackbird.Domain/Entities/LawyerDashboardInfo.cs
+++ src/Blackbird.Domain/Entities/LawyerDashboardInfo.cs
@@ -5,16 +5,22 @@ namespace Blackbird.Domain.Entities
     public class LawyerDashboardInfo : EntityBase
     {
         #region private declarations
-        private long lawyersTopRecommendId;
+        private long lawyerDashboardInfoId;
         private long userId;
-        private long categoryId; // if 0 then he is universally recommended, else in a particular category
+        private long current_MembershipPlanID;
+        private DateOnly current_MembershipPlan_Registered_On;
+        private DateOnly current_MembershipPlan_Expires_On;
+        private long total_Clients_View_Profile;
+        private long total_Clients_Message_Through_Website;
+        private long total_Clients_Message_Through_Whatsapp;
+        private long total_Clients_Message_Through_Facebook;
         #endregion
 
-        public long LawyersTopRecommendId
+        public long LawyerDashboardInfoId
         {
-            get => lawyersTopRecommendId; set
+            get => lawyerDashboardInfoId; set
             {
-                lawyersTopRecommendId = value;
+                lawyerDashboardInfoId = value;
                 EntityModified();
             }
         }
@@ -25,6 +31,54 @@ namespace Blackbird.Domain.Entities
                 userId = value; EntityModified();
             }
         }
-
+        public long Current_MembershipPlanID
+        {
+            get => current_MembershipPlanID; set
+            {
+                current_MembershipPlanID = value; EntityModified();
+            }
+        }
+        public DateOnly Current_MembershipPlan_Registered_On
+        {
+            get => current_MembershipPlan_Registered_On; set
+            {
+                current_MembershipPlan_Registered_On = value; EntityModified();
+            }
+        }
+        public DateOnly Current_MembershipPlan_Expires_On
+        {
+            get => current_MembershipPlan_Expires_On; set
+            {
+                current_MembershipPlan_Expires_On = value; EntityModified();
+            }
+        }
+        public long Total_Clients_View_Profile
+        {
+            get => total_Clients_View_Profile; set
+            {
+                total_Clients_View_Profile = value; EntityModified();
+            }
+        }
+        public long Total_Clients_Message_Through_Website
+        {
+            get => total_Clients_Message_Through_Website; set
+            {
+                total_Clients_Message_Through_Website = value; EntityModified();
+            }
+        }
+        public long Total_Clients_Message_Through_Whatsapp
+        {
+            get => total_Clients_Message_Through_Whatsapp; set
+            {
+                total_Clients_Message_Through_Whatsapp = value; EntityModified();
+            }
+        }
+        public long Total_Clients_Message_Through_Facebook
+        {
+            get => total_Clients_Message_Through_Facebook; set
+            {
+                total_Clients_Message_Through_Facebook = value; EntityModified();
+            }
+        }
     }
 }
